b410ba5ecf2747f41bd1570caf63814fc2d3d623
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-21 09:18:14 -0700

    added

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,6 @@
+const pgConnection =
+  process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits";
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -12,12 +15,13 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
-  testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/test.db3",
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
     },
-    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations",
     },
